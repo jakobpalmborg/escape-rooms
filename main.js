3d06956919a657373c0048d35fb465cea0af53fd
jakobpalmborg <jakob.palmborg@gmail.com>
2022-11-10 08:24:48 +0100

    add hamburger-cross

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const hamburgerBtn = document.querySelector('.hamburger-btn');
 
-hamburgerBtn.addEventListener('click', openHamburgerMeny);
+hamburgerBtn.addEventListener('click', openOrCloseHamburgerMeny);
 
 const navList = document.querySelector('nav');
 console.log(navList);
 
-function openHamburgerMeny() {
-    navList.classList.add('nav-list-show');
-    hamburgerBtn.classList.add('hamburger-cross')
-    console.log('clicked');
-    console.log(navList);
+function openOrCloseHamburgerMeny() {
+    if(navList.classList.contains('nav-list-show')) {
+        navList.classList.remove('nav-list-show');
+    } else {
+        navList.classList.add('nav-list-show');
+        
+    }
+    
+    
 }
+
+
+// hamburgerBtn.classList.add('hamburger-cross')
+//     console.log('clicked');
+//     console.log(navList);
