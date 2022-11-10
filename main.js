512ad96ca4f13e3d713c807dbfc27dc6a883a67f
jakobpalmborg <jakob.palmborg@gmail.com>
2022-11-10 10:40:47 +0100

    nest mobil meny in scss

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
 const hamburgerBtn = document.querySelector('.hamburger-btn');
-
 hamburgerBtn.addEventListener('click', openHamburgerMeny);
-
 const navList = document.querySelector('nav');
-
 const hamburgerCrossBtn = document.querySelector('.hamburger-cross');
-
 hamburgerCrossBtn.addEventListener('click', closeHamburgerMeny)
+const body = document.querySelector('body');
+const header = document.querySelector('header')
+
+
 
 function openHamburgerMeny() {
+    navList.classList.add('nav-list-show');
+    body.classList.add('fade-out-animation');
     
-        navList.classList.add('nav-list-show');
-        
-    }
-      
-
-
+}
+ 
 function closeHamburgerMeny() {
     navList.classList.remove('nav-list-show');
 }
-
-
-// hamburgerBtn.classList.add('hamburger-cross')
-//     console.log('clicked');
-//     console.log(navList);
